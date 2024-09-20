515d391c5b816855efe4c6b2150422dea2012520
Eric <estraw@smu.edu>
2024-09-20 06:58:31 -0500

    Tutorial features tried out

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,6 @@
-import { Text, View } from "react-native";
+import { Alert, Button, Text, TouchableWithoutFeedback, TouchableOpacity, TouchableHighlight, Image, View } from "react-native";
 
 export default function App() {
-  let x;
-  console.log(x);
   return (
     <View
       style={{
@@ -12,6 +10,27 @@ export default function App() {
       }}
     >
       <Text>Hello World: My First App</Text>
+      <Image source={require('../assets/images/react-logo.png')}/>
+      <TouchableHighlight onPress={() => console.log("Image Tapped")}>
+        <Image 
+        source={{
+          width: 200,
+          height: 300,
+          uri: 'https://picsum.photos/200/300'}
+        }/>
+      </TouchableHighlight>
+      <Button
+        color="orange"
+        title="Press me"
+        onPress={() => Alert.alert('Simple Button pressed')}
+        />
+        <View
+          style={{
+            backgroundColor: 'red',
+          }}
+        >
+          <Text>Some text that is really really really really long text that will use many many many many line</Text>
+        </View>
     </View>
   );
 }
